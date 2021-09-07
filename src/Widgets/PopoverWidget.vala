casasfernando/wingpanel-indicator-airpods

commit 3fec538f5f5e8d63413c790330b997c4a8476eed

Fixed a bug related to battery saver mode warning display in popover menu

src/Widgets/PopoverWidget.vala
@@ -197,7 +197,7 @@ namespace WingpanelAirPods {
         }
 
         public void update_batt_warn_visibility () {
-            if (settings.get_boolean ("battery-saver-mode") && settings.get_boolean ("system-on-battery")) {
+            if ((settings.get_int ("battery-saver-mode") > 0) && settings.get_boolean ("system-on-battery")) {
                 set_widget_visible (batt_saver_warn, true);
                 set_widget_visible (batt_saver_warn_separator, true);
             } else {

commit 953220b9549eb2adaf22966d60cff58e1f5e1580

Fixed a bug related to battery saver mode warning display in popover menu introduced in commit 3fec538

src/Widgets/PopoverWidget.vala
@@ -198,7 +198,7 @@ namespace WingpanelAirPods {
         }
 
         public void update_batt_warn_visibility () {
-            if ((settings.get_int ("battery-saver-mode") > 0) && settings.get_boolean ("system-on-battery")) {
+            if (settings.get_boolean ("battery-saver-mode-engaged")) {
                 set_widget_visible (batt_saver_warn, true);
                 set_widget_visible (batt_saver_warn_separator, true);
             } else {